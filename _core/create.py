437306039ede0a371b7876fe1dabdff4df045e1f
Nils Urbach <ndu01u@gmail.com>
2023-09-19 13:22:04 +0200

    save

--- _core/create.py
+++ _core/create.py
@@ -3,7 +3,6 @@ from datetime import date, datetime
 from picdir import PicDir
 from argparse import ArgumentParser, Namespace
 
-
 # TODO: write a ArgumentParser for the arguments of the create command
 # TODO: code the create command (it creates a new FotoDir)
 
@@ -13,11 +12,22 @@ epilog = ""
 
 
 def create_parser_factory(parser: ArgumentParser) -> None:
-    raise NotImplementedError()
+    parser.add_argument("name",
+                        help="name of the new picdir")
+    parser.add_argument("-dir", "--parent-dir",
+                        help=f"where to create the new picdir (default: {str(default_dir)}",
+                        type=Path,
+                        default=default_dir)
+    parser.add_argument("-d", "--date",
+                        help="the date (DD-MM-YY) of the new picdir (default: today)",
+                        type=lambda s: datetime.strptime(s, CreateParser._DATE_FORMAT),
+                        default=date.today())
+    parser.add_argument("-s", "--source",
+                        help="pictures to add in the new picdir",
+                        type=Path)
 
 
 class CreateParser(ArgumentParser):
-
     _DATE_FORMAT = "%d-%m-%y"
 
     def __init__(self, default_dir: Path):
@@ -25,19 +35,6 @@ class CreateParser(ArgumentParser):
                                 prog=command_name,
                                 description=description,
                                 epilog=epilog)
-        self.add_argument("name",
-                          help="name of the new picdir")
-        self.add_argument("-dir", "--parent-dir",
-                          help=f"where to create the new picdir (default: {str(default_dir)}",
-                          type=Path,
-                          default=default_dir)
-        self.add_argument("-d", "--date",
-                          help="the date (DD-MM-YY) of the new picdir (default: today)",
-                          type=lambda s: datetime.strptime(s, CreateParser._DATE_FORMAT),
-                          default=date.today())
-        self.add_argument("-s", "--source",
-                          help="pictures to add in the new picdir",
-                          type=Path)
 
 
 def create(args: Namespace) -> PicDir:
